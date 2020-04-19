848cc0a50f5b9ba64c97672da4301841cbd31687
Tomas Fabian <tomas.fabian@colosseoeas.com>
2020-04-19 16:11:14 +0200

    [Joker] - Enumerable extensions Do, ForEach, Consume

--- Joker/Extensions/EnumerableExtensions.cs
+++ Joker/Extensions/EnumerableExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Joker.Extensions
@@ -10,6 +11,60 @@ namespace Joker.Extensions
     public static bool IsOneOfFollowing<TItem>(this TItem item, params TItem[] allowedValues)
     {
       return allowedValues.Any(c => c.Equals(item));
+    }
+
+    #endregion
+
+    #region Consume
+
+    public static void Consume<TItem>(this IEnumerable<TItem> source)
+    {
+      if (source == null) throw new ArgumentNullException(nameof(source));
+
+      foreach (var _ in source)
+      {
+      }
+    }
+
+    #endregion
+
+    #region ForEach
+
+    public static void ForEach<TItem>(this IEnumerable<TItem> source, Action<TItem> action)
+    {
+      if (source == null) throw new ArgumentNullException(nameof(source));
+
+      foreach (var item in source)
+      {
+        action(item);
+      }
+    }
+    public static void ForEach<TItem>(this IEnumerable<TItem> source, Action<TItem, long> action)
+    {
+      if (source == null) throw new ArgumentNullException(nameof(source));
+
+      long index = 0;
+
+      foreach (var item in source)
+      {
+        action(item, ++index);
+      }
+    }
+
+    #endregion
+
+    #region Do
+
+    public static IEnumerable<TItem> Do<TItem>(this IEnumerable<TItem> source, Action<TItem> action)
+    {
+      if (source == null) throw new ArgumentNullException(nameof(source));
+
+      foreach (var item in source)
+      {
+        action(item);
+
+        yield return item;
+      }
     }
 
     #endregion
