94a3a07319b747ed1a5d30fc965a2afcf56830b6
Tomas Fabian <tomas.fabian@colosseoeas.com>
2020-04-23 08:44:22 +0200

    [Joker.OData] - SqlTableDependencyRedisProvider as HostedService

---
+++ Samples/OData/SelfHostedODataService/Configuration/ConfigurationProvider.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using Joker.Extensions;
+
+namespace SelfHostedODataService.Configuration
+{
+  public class ConfigurationProvider
+  {
+    #region GetDatabaseConnectionString
+
+    public static string GetDatabaseConnectionString()
+    {
+      var connectionString = ConfigurationManager.ConnectionStrings["FargoEntities"].ConnectionString;
+
+      var configuration = Environment.GetEnvironmentVariables();
+      var host = configuration["DBHOST"] as string;
+
+      if (host.IsNullOrEmpty())
+      {
+        return connectionString;
+      }
+      
+      var sqlConnectionStringBuilder = new SqlConnectionStringBuilder
+      {
+        ConnectionString = connectionString,
+        DataSource = host
+      };
+
+      return sqlConnectionStringBuilder.ConnectionString;
+    }
+
+    #endregion
+
+    #region RedisUrl
+
+    public static string RedisUrl
+    {
+      get
+      {
+        var configuration = Environment.GetEnvironmentVariables();
+
+        var redisUrl = configuration["REDISHOST"] as string;
+
+        redisUrl = redisUrl ?? ConfigurationManager.AppSettings["RedisUrl"];
+
+        return redisUrl ?? "localhost";
+      }
+    }
+
+    #endregion
+  }
+}
---
+++ Samples/OData/SelfHostedODataService/HostedServices/SqlTableDependencyProviderHostedService.cs
@@ -0,0 +1,76 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Joker.Factories.Schedulers;
+using Joker.Redis.ConnectionMultiplexers;
+using Microsoft.Extensions.Hosting;
+using Sample.Data.SqlTableDependencyProvider;
+using SelfHostedODataService.Configuration;
+using SelfHostedODataService.Redis;
+using SqlTableDependency.Extensions.Enums;
+
+namespace SelfHostedODataService.HostedServices
+{
+  internal class SqlTableDependencyProviderHostedService : IHostedService
+  {
+    private readonly IHostApplicationLifetime appLifetime;
+
+    public SqlTableDependencyProviderHostedService(
+      IHostApplicationLifetime appLifetime)
+    {
+      this.appLifetime = appLifetime;
+    }
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+      appLifetime.ApplicationStarted.Register(OnStarted);
+      appLifetime.ApplicationStopping.Register(OnStopping);
+      appLifetime.ApplicationStopped.Register(OnStopped);
+
+      return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+      return Task.CompletedTask;
+    }
+
+    private void OnStarted()
+    {
+      InitializeSqlTableDependencyRedisProvider();
+    }
+
+    #region InitializeSqlTableDependencyRedisProvider
+
+    private ProductSqlTableDependencyRedisProvider redisPublisher;
+
+    private void InitializeSqlTableDependencyRedisProvider()
+    {
+      string connectionString = ConfigurationProvider.GetDatabaseConnectionString();
+
+      var schedulersFactory = new SchedulersFactory();
+      var productsChangesProvider =
+        new ProductsSqlTableDependencyProvider(connectionString, schedulersFactory.TaskPool, LifetimeScope.UniqueScope);
+      productsChangesProvider.SubscribeToEntityChanges();
+
+
+      string redisUrl = ConfigurationProvider.RedisUrl;
+      
+      redisPublisher = new ProductSqlTableDependencyRedisProvider(productsChangesProvider,
+        new RedisPublisher(redisUrl), schedulersFactory.TaskPool);
+      
+      redisPublisher.StartPublishing();
+    }
+
+    #endregion
+
+    private void OnStopping()
+    {
+      redisPublisher?.Dispose();
+    }
+
+    private void OnStopped()
+    {
+
+    }
+  }
+}
