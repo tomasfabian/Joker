a5e6417fb1e85dcb8367796ac357c7fdb6ec91e8
Tomas Fabian <tomas.fabian@colosseoeas.com>
2020-04-28 14:00:52 +0200

    [Joker.OData] - code improvements

---
+++ Joker.OData/Startup/StartupSettings.cs
@@ -0,0 +1,29 @@
+﻿namespace Joker.OData.Startup
+{
+  public class StartupSettings
+  {
+    public bool UseHttpsRedirection { get; private set; } = true;
+
+    public bool UseUtcTimeZone { get; private set; } = true;
+
+    public bool UseDeveloperExceptionPage { get; set; } = true;
+
+    public bool UseAuthorization { get; set; } = true;
+
+    public bool IISAllowSynchronousIO { get; set; } = true;
+    
+    public StartupSettings DisableHttpsRedirection()
+    {
+      UseHttpsRedirection = false;
+
+      return this;
+    }
+
+    public StartupSettings DoNotUseUtcTimeZone()
+    {
+      UseUtcTimeZone = false;
+
+      return this;
+    }
+  }
+}
---
+++ Joker.OData/Startup/WebApiStartupSettings.cs
@@ -0,0 +1,49 @@
+﻿using Joker.Extensions;
+
+namespace Joker.OData.Startup
+{
+  public class WebApiStartupSettings
+  {    
+    public string WebApiRouteName { get; private set; } = "WebApiRoute";
+    public string WebApiRoutePrefix { get; private set; } = "api";
+    public string WebApiTemplate { get; private set; } = "{controller}/{action}/{id?}";
+
+    public string WebApiRoute
+    {
+      get
+      {
+        if(WebApiRoutePrefix.IsNullOrEmpty() && WebApiTemplate.IsNullOrEmpty())
+          return string.Empty;
+
+        if(WebApiRoutePrefix.IsNullOrEmpty())
+          return WebApiTemplate;
+
+        if(WebApiTemplate.IsNullOrEmpty())
+          return $"{WebApiRoutePrefix}/";
+        
+        return $"{WebApiRoutePrefix}/{WebApiTemplate}";
+      }
+    }
+
+    public WebApiStartupSettings SetWebApiRouteName(string value)
+    {
+      WebApiRouteName = value;
+
+      return this;
+    }
+
+    public WebApiStartupSettings SetWebApiRoutePrefix(string webApiRoutePrefix)
+    {
+      WebApiRoutePrefix = webApiRoutePrefix;
+
+      return this;
+    }
+
+    public WebApiStartupSettings SetWebApiTemplate(string webApiTemplate)
+    {
+      WebApiTemplate = webApiTemplate;
+
+      return this;
+    }
+  }
+}
