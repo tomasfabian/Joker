e08726554dbbbe8dc722be61664ad1f86635541c
Tomas Fabian <tomas.fabian@colosseoeas.com>
2020-06-05 14:46:39 +0200

    [WinUI3.Sample] - PlatformSchedulersFactory and DispatcherScheduler

---
+++ Samples/WinUI3/Joker.WinUI3.Sample/Joker.WinUI3.Sample/Factories/Schedulers/IPlatformSchedulersFactory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Reactive.Concurrency;
+
+//namespace Joker.WinUI3.Sample.Factories.Schedulers
+namespace Joker.WPF.Sample.Factories.Schedulers
+{
+  public interface IPlatformSchedulersFactory
+  {
+    IScheduler Dispatcher { get;}
+  }
+}
---
+++ Samples/WinUI3/Joker.WinUI3.Sample/Joker.WinUI3.Sample/Factories/Schedulers/PlatformSchedulersFactory.cs
@@ -0,0 +1,31 @@
+﻿using Joker.Factories.Schedulers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Concurrency;
+
+//namespace Joker.WinUI3.Sample.Factories.Schedulers
+namespace Joker.WPF.Sample.Factories.Schedulers
+{
+  public class PlatformSchedulersFactory : SchedulersFactory, IPlatformSchedulersFactory
+  {
+    private readonly object dispatcherGate = new object();
+
+    private IScheduler dispatcher;
+
+    public IScheduler Dispatcher
+    {
+      get
+      {
+        lock (dispatcherGate)
+        {
+          if (dispatcher == null)
+            dispatcher = DispatcherScheduler.Current;
+
+          return dispatcher;
+        }
+      }
+    }
+  }
+}
