cf9817834a1cfd2cd80b2e682e7af98af7eb2244
Tomas Fabian <tomas.fabian@colosseoeas.com>
2020-06-18 13:36:15 +0200

    [WinUI3.UWP.Sample] - dialog manager

---
+++ Samples/WinUI3/Joker.WinUI3.UWP.Sample/Navigation/DialogManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Joker.PubSubUI.Shared.Navigation;
+using Microsoft.UI.Xaml.Controls;
+
+namespace Joker.WinUI3.UWP.Sample.Navigation
+{
+  public class DialogManager : IDialogManager
+  {
+    public void ShowMessage(string message)
+    {
+      ContentDialog dialog = new ContentDialog()
+      {
+        Title = "Error",
+        Content = message,
+        CloseButtonText = "Ok"
+      };
+
+      dialog.ShowAsync();
+    }
+  }
+}
