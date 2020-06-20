2c08e19f053e1317cd877bf3d858e4259114741e
Tomas Fabian <tomas.fabian@colosseoeas.com>
2020-06-20 11:28:18 +0200

    [WinUI3.UWP.Sample] - shared views and code usage in UWP

--- Samples/WinUI3/Joker.WinUI3.UWP.Sample/MainPage.xaml.cs
+++ Samples/WinUI3/Joker.WinUI3.UWP.Sample/MainPage.xaml.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Joker.PubSubUI.Shared.ViewModels.Products;
+using Joker.Redis.Notifications;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -12,24 +14,27 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using Ninject;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace Joker.WinUI3.UWP.Sample
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainPage : Page
+  /// <summary>
+  /// An empty page that can be used on its own or navigated to within a Frame.
+  /// </summary>
+  public sealed partial class MainPage : Page
+  {
+    public MainPage()
     {
-        public MainPage()
-        {
-            this.InitializeComponent();
-        }
+      this.InitializeComponent();
 
-        private void myButton_Click(object sender, RoutedEventArgs e)
-        {
-            myButton.Content = "Clicked";
-        }
+      var productsEntityChangesViewModel = App.Kernel.Get<ProductsEntityChangesViewModel>();
+
+      IDomainEntitiesSubscriber domainEntitiesSubscriber = App.Kernel.Get<IDomainEntitiesSubscriber>();
+      domainEntitiesSubscriber.Subscribe();
+
+      ProductsView.ViewModel = productsEntityChangesViewModel;
     }
+  }
 }
