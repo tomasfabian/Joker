5ac07725941e374bc5f7261c722c9d887cf5df71
Tomas Fabian <tomas.fabian@colosseoeas.com>
2020-06-25 08:17:13 +0200

    [Blazor.Wasm] - modal dialog

---
+++ Samples/Blazor.WebAssembly/Joker.BlazorApp.Sample/Pages/ModalDialogBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace Joker.BlazorApp.Sample.Pages
+{
+  public class ModalDialogBase : ComponentBase
+  {
+    public ModalDialogBase()
+    {
+      ShowPopup = true;
+    }
+
+    public bool ShowPopup { get; set; }
+    public string Message { get; set; }
+
+    public void ClosePopup()
+    {
+      ShowPopup = false;
+    }
+  }
+}
