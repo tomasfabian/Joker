ca40bf826e1ae61fd596cac464aa5d46198a5592
Tomas Fabian <tomas.fabian@colosseoeas.com>
2020-06-26 16:54:35 +0200

    [Blazor.Wasm] - SignalR retry policy

---
+++ Samples/Blazor.WebAssembly/Joker.BlazorApp.Sample/Subscribers/RandomRetryPolicy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace Joker.BlazorApp.Sample.Subscribers
+{
+  public class RandomRetryPolicy : IRetryPolicy
+  {
+    public TimeSpan? NextRetryDelay(RetryContext retryContext)
+    {
+      if (retryContext.ElapsedTime < TimeSpan.FromSeconds(60))
+      {
+        Console.WriteLine("reconnecting 10s");
+        return TimeSpan.FromSeconds(10);
+      }
+      
+      Console.WriteLine("reconnecting 1m");
+      return TimeSpan.FromMinutes(1);
+    }
+  }
+}
