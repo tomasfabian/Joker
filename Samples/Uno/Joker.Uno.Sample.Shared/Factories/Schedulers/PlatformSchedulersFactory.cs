737959c7c4c271d3b2a6d91cbce206ac5b1e8c3e
Tomas Fabian <tomas.fabian@colosseoeas.com>
2020-07-04 19:25:13 +0200

    [Joker.Uno.Sample] - shared uno project

---
+++ Samples/Uno/Joker.Uno.Sample.Shared/Factories/Schedulers/PlatformSchedulersFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reactive.Concurrency;
+using Joker.Factories.Schedulers;
+using Joker.Platforms.Factories.Schedulers;
+
+namespace Joker.Uno.Sample.Shared.Factories.Schedulers
+{
+  public class PlatformSchedulersFactory : SchedulersFactory, IPlatformSchedulersFactory
+  {
+    private readonly object dispatcherGate = new object();
+
+    private IScheduler dispatcher;
+
+    public IScheduler Dispatcher
+    {
+      get
+      {
+        lock (dispatcherGate)
+        {
+          if (dispatcher == null)
+            dispatcher = new CoreDispatcherScheduler(Windows.UI.Xaml.Window.Current.Dispatcher);
+
+          return dispatcher;
+        }
+      }
+    }
+  }
+}
