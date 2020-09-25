14b70ced625ccda944a8dc3d94fab54c33c71119
Tomas Fabian <tomas.fabian@colosseoeas.com>
2020-09-25 08:14:01 +0200

    [Sample.Data.EFCore] - Products table update timestamp trigger

---
+++ Samples/Sample.DataCore/Migrations/20200924100254_Timestamp_Triggers.cs
@@ -0,0 +1,12 @@
+﻿using Sample.Domain.Models;
+
+namespace Sample.DataCore.Migrations
+{
+  public partial class Timestamp_Triggers : CreatedAndModifiedMigration
+  {
+    protected override string[] DomainEntities =>
+      new[] {
+        nameof(Product),
+      };
+  }
+}
---
+++ Samples/Sample.DataCore/Migrations/CreatedAndModifiedMigration.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Joker.Domain;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Pluralize.NET;
+
+namespace Sample.DataCore.Migrations
+{
+  public abstract class CreatedAndModifiedMigration : Migration
+  {
+    private static readonly IPluralize EnglishPluralizationService = new Pluralizer();
+
+    protected bool AddBeforeInsertTrigger { get; set; }
+
+    protected abstract string[] DomainEntities { get; }
+
+    private IEnumerable<string> PluralizedEntityNames => DomainEntities.Select(domainEntity => EnglishPluralizationService.Pluralize(domainEntity));
+
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {      
+      foreach (var entitySetName in PluralizedEntityNames)
+      {
+        if(AddBeforeInsertTrigger)
+          migrationBuilder.Sql(CreateTrigger(entitySetName, "insert", nameof(DomainEntity.Timestamp)));
+        
+        migrationBuilder.Sql(CreateTrigger(entitySetName, "update", nameof(DomainEntity.Timestamp)));
+      }
+    }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+      foreach (var entitySetName in PluralizedEntityNames)
+      {
+        migrationBuilder.Sql(CreateDropTrigger(entitySetName + "BI"));
+        migrationBuilder.Sql(CreateDropTrigger(entitySetName + "BU"));
+      }
+    }
+
+    public string CreateDropTrigger(string triggerName)
+    {
+      return $"DROP TRIGGER [{triggerName}]";
+    }
+
+    public string CreateTrigger(string tableName, string operation, string columnName)
+    {
+      string triggerName = tableName + (operation == "insert" ? "BI" : "BU");
+
+      return $@"CREATE trigger[dbo].[{triggerName}] on[dbo].[{tableName}] for {operation} as
+    begin
+      declare
+    @numrows int
+
+      select  @numrows = @@rowcount
+    if @numrows = 0
+    return
+
+    UPDATE {tableName} SET {columnName} = GETDATE()
+    FROM Inserted i
+      WHERE i.Id = {tableName}.Id   
+
+    return
+
+    error:
+      rollback transaction
+    end";
+    }
+  }
+}
