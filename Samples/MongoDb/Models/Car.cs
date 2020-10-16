00c3e82d3956e4df29336f05e660d4a6487d4432
Tomas Fabian <tomas.fabian@colosseoeas.com>
2020-10-16 21:45:17 +0200

    [Joker.Samples] - AspNetCore.MongoDb example - models

---
+++ Samples/MongoDb/Models/Car.cs
@@ -0,0 +1,10 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace Colosseo.MagicPlatform.Models
+{
+  public class Car : DomainEntity
+  {
+    [BsonElement("Name")]
+    public string CarName { get; set; }
+  }
+}
---
+++ Samples/MongoDb/Models/DomainEntity.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Colosseo.MagicPlatform.Models
+{
+  public class DomainEntity
+  {
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+  }
+}
