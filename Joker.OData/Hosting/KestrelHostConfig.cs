acf91875308795aae7616549a615e45402db168d
Tomas Fabian <tomas.fabian@colosseoeas.com>
2020-10-19 08:13:33 +0200

    [Joker.OData] - api host configs

---
+++ Joker.OData/Hosting/KestrelHostConfig.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+
+namespace Joker.OData.Hosting
+{
+  public class KestrelHostConfig : WebHostConfig
+  {
+    public Action<KestrelServerOptions> ConfigureKestrelServer { get; set; }
+  }
+}
