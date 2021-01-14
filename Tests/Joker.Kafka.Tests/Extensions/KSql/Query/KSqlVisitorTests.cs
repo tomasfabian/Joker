6f9d821af97a82a5babd404a988f8c277dacc4fb
Tomas Fabian <tomas.fabian@colosseoeas.com>
2021-01-14 20:52:36 +0100

    [Joker.Kafka] - KSqlVisitor Unit Tests - constants

--- Tests/Joker.Kafka.Tests/Extensions/KSql/Query/KSqlVisitorTests.cs
+++ Tests/Joker.Kafka.Tests/Extensions/KSql/Query/KSqlVisitorTests.cs
@@ -1,4 +1,6 @@
-﻿using Joker.Kafka.Extensions.KSql.Query;
+﻿using System.Linq.Expressions;
+using FluentAssertions;
+using Joker.Kafka.Extensions.KSql.Query;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using UnitTests;
 
@@ -14,5 +16,78 @@ namespace Joker.Kafka.Tests.Extensions.KSql.Query
 
         ClassUnderTest = new KSqlVisitor();
       }
+
+      #region Constants
+
+      [TestMethod]
+      public void NullConstant_BuildKSql_PrintsStringifiedNull()
+      {
+        //Arrange
+        var constantExpression = Expression.Constant(null);
+
+        //Act
+        var query = ClassUnderTest.BuildKSql(constantExpression);
+
+        //Assert
+        query.Should().BeEquivalentTo("NULL");
+      }
+
+      [TestMethod]
+      public void TextConstant_BuildKSql_PrintsTextSurroundedWithTicks()
+      {
+        //Arrange
+        var constant = "TeSt Me";
+        var constantExpression = Expression.Constant(constant);
+
+        //Act
+        var query = ClassUnderTest.BuildKSql(constantExpression);
+
+        //Assert
+        query.Should().BeEquivalentTo($"'{constant}'");
+      }
+
+      [TestMethod]
+      public void ValueTypeConstant_BuildKSql_PrintsPlainText()
+      {
+        //Arrange
+        var constant = 42;
+        var constantExpression = Expression.Constant(constant);
+
+        //Act
+        var query = ClassUnderTest.BuildKSql(constantExpression);
+
+        //Assert
+        query.Should().BeEquivalentTo(constant.ToString());
+      }
+
+      [TestMethod]
+      public void EnumerableConstant_BuildKSql_PrintsCommaSeparatedTextFields()
+      {
+        //Arrange
+        var constant = new[] { "Field1", "Field2" };
+        var constantExpression = Expression.Constant(constant);
+
+        //Act
+        var query = ClassUnderTest.BuildKSql(constantExpression);
+
+        //Assert
+        query.Should().BeEquivalentTo("Field1, Field2");
+      }
+
+      [TestMethod]
+      public void ReferenceTypeConstant_BuildKSql_PrintsCommaSeparatedTextFields()
+      {
+        //Arrange
+        var constant = new object();
+        var constantExpression = Expression.Constant(constant);
+
+        //Act
+        var query = ClassUnderTest.BuildKSql(constantExpression);
+
+        //Assert
+        query.Should().BeEquivalentTo(constant.ToString());
+      }
+
+      #endregion
   }
 }
