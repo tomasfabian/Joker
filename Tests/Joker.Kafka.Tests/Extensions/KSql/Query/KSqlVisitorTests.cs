44a883a6de040bcbf1f613a742d5cc77d18ba808
Tomas Fabian <tomas.fabian@colosseoeas.com>
2021-01-14 20:33:50 +0100

    [Joker.Kafka] - Unit Tests project

---
+++ Tests/Joker.Kafka.Tests/Extensions/KSql/Query/KSqlVisitorTests.cs
@@ -0,0 +1,18 @@
+﻿using Joker.Kafka.Extensions.KSql.Query;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTests;
+
+namespace Joker.Kafka.Tests.Extensions.KSql.Query
+{
+  [TestClass]
+  public class KSqlVisitorTests : TestBase<KSqlVisitor>
+  {
+      [TestInitialize]
+      public override void TestInitialize()
+      {
+        base.TestInitialize();
+
+        ClassUnderTest = new KSqlVisitor();
+      }
+  }
+}
