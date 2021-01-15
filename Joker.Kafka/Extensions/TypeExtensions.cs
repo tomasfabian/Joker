51f307c2dfe32716ebbda330a8b5a6b6faba4c0b
Tomas Fabian <tomas.fabian@colosseoeas.com>
2021-01-15 17:59:16 +0100

    [Joker.Kafka] - TypeExtensions - Is anonymous type

---
+++ Joker.Kafka/Extensions/TypeExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Joker.Kafka.Extensions
+{
+  internal static class TypeExtensions
+  {
+    internal static bool IsAnonymousType(this Type type)
+      => type.Name.StartsWith("<>", StringComparison.Ordinal)
+         && type.GetCustomAttributes(typeof(CompilerGeneratedAttribute), inherit: false).Length > 0
+         && type.Name.Contains("AnonymousType");
+  }
+}
