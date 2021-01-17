cc6acd0fdbdcc09f2ff0884af030725b274cfa29
Tomas Fabian <tomas.fabian@colosseoeas.com>
2021-01-17 09:44:18 +0100

    [Joker.Kafka] - IQbservableProvider and IQbservable<> interfaces

---
+++ Joker.Kafka/Extensions/KSql/Linq/IQbservable.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Joker.Kafka.Extensions.ksql.Linq
+{
+  public interface IQbservable
+  {
+    /// <summary>
+    /// Gets the type of the element(s) that are returned when the expression tree associated with this instance of IQbservable is executed.
+    /// </summary>
+    Type ElementType { get; }
+
+    /// <summary>
+    /// Gets the expression tree that is associated with the instance of IQbservable.
+    /// </summary>
+    Expression Expression { get; }
+
+    /// <summary>
+    /// Gets the query provider that is associated with this data source.
+    /// </summary>
+    IQbservableProvider Provider { get; }
+  }
+
+  public interface IQbservable<out T> : IQbservable, IObservable<T>
+  {
+  }
+}
---
+++ Joker.Kafka/Extensions/KSql/Linq/IQbservableProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Joker.Kafka.Extensions.ksql.Linq
+{
+  public interface IQbservableProvider
+  {
+    /// <summary>
+    /// Constructs an <see cref="IQbservable{T}"/> object that can evaluate the query represented by a specified expression tree.
+    /// </summary>
+    /// <typeparam name="TResult">The type of the elements of the <see cref="IQbservable{T}"/> that is returned.</typeparam>
+    /// <param name="expression">Expression tree representing the query.</param>
+    /// <returns>IQbservable object that can evaluate the given query expression.</returns>
+    IQbservable<TResult> CreateQuery<TResult>(Expression expression);
+  }
+}
