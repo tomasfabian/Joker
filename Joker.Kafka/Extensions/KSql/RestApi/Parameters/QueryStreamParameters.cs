252bd242474456163963603a6753f00113b088fb
Tomas Fabian <tomas.fabian@colosseoeas.com>
2021-01-18 21:36:22 +0100

    [Kafka.DotNet.ksqlDB] - query stream rest api parameters, responses, exception

---
+++ Joker.Kafka/Extensions/KSql/RestApi/Parameters/QueryStreamParameters.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace Kafka.DotNet.ksqlDB.Extensions.KSql.RestApi.Parameters
+{
+  public sealed class QueryStreamParameters
+  {
+    [JsonPropertyName("sql")]
+    public string Sql { get; set; }
+
+    [JsonPropertyName("properties")]
+    public Dictionary<string, string> Properties { get; } = new();
+
+    public string this[string key]
+    {
+      get => Properties[key];
+      set => Properties[key] = value;
+    }
+  }
+}
