cdd4dd8d0cad51a6f409c81db2c3ba2d5c898845
Tomas Fabian <tomas.fabian@colosseoeas.com>
2021-01-23 12:19:25 +0100

    [Kafka.DotNet.ksqlDB] - Record not released in nuget package, temporarily commented in the example

--- Samples/Kafka/Kafka.DotNet.ksqlDB.Sample/Model/Tweet.cs
+++ Samples/Kafka/Kafka.DotNet.ksqlDB.Sample/Model/Tweet.cs
@@ -1,10 +1,12 @@
 ﻿using System.Text.Json.Serialization;
-using Kafka.DotNet.ksqlDB.Extensions.KSql.Query;
+//using Kafka.DotNet.ksqlDB.Extensions.KSql.Query;
 
 namespace Kafka.DotNet.ksqlDB.Sample.Model
 {
-  public class Tweet : Record
-  {
+  public class Tweet //: Record not released in nuget package
+  {    
+    public long RowTime { get; set; }
+
     public int Id { get; set; }
 
     [JsonPropertyName("MESSAGE")]
