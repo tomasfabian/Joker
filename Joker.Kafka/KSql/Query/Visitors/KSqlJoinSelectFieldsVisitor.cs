7d8143c0fac542c858cac28ceaa4ff842c0f3584
Tomas Fabian <tomas.fabian@colosseoeas.com>
2021-02-03 07:53:53 +0100

    [Kafka.DotNet.ksqlDB] - Inner join visitors

---
+++ Joker.Kafka/KSql/Query/Visitors/KSqlJoinSelectFieldsVisitor.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+
+namespace Kafka.DotNet.ksqlDB.KSql.Query.Visitors
+{
+  internal sealed class KSqlJoinSelectFieldsVisitor : KSqlVisitor
+  {
+    internal KSqlJoinSelectFieldsVisitor(StringBuilder stringBuilder)
+      : base(stringBuilder, useTableAlias: true)
+    {
+    }
+
+    protected override void ProcessVisitNewMember((MemberInfo memberInfo, Expression expresion) v)
+    {
+      if (v.expresion.NodeType == ExpressionType.MemberAccess)
+      {
+        Visit(v.expresion);
+              
+        Append(" " + v.memberInfo.Name);
+      }
+      else
+      {
+        base.ProcessVisitNewMember(v);
+      }
+    }
+
+    protected override Expression VisitMember(MemberExpression memberExpression)
+    {
+      if (memberExpression == null) throw new ArgumentNullException(nameof(memberExpression));
+
+      if (memberExpression.Expression.NodeType == ExpressionType.Parameter)
+      {
+        Append(((ParameterExpression)memberExpression.Expression).Name);
+        Append(".");
+        Append(memberExpression.Member.Name);
+      }
+      else
+        base.VisitMember(memberExpression);
+
+      return memberExpression;
+    }
+  }
+}
---
+++ Joker.Kafka/KSql/Query/Visitors/KSqlJoinsVisitor.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using Kafka.DotNet.ksqlDB.Infrastructure.Extensions;
+using Kafka.DotNet.ksqlDB.KSql.Query.Context;
+using Pluralize.NET;
+
+namespace Kafka.DotNet.ksqlDB.KSql.Query.Visitors
+{
+  internal class KSqlJoinsVisitor : KSqlVisitor
+  {
+    private readonly KSqlDBContextOptions contextOptions;
+    private readonly QueryContext queryContext;
+
+    public KSqlJoinsVisitor(StringBuilder stringBuilder, KSqlDBContextOptions contextOptions, QueryContext queryContext)
+      : base(stringBuilder, useTableAlias: false)
+    {
+      this.contextOptions = contextOptions ?? throw new ArgumentNullException(nameof(contextOptions));
+      this.queryContext = queryContext ?? throw new ArgumentNullException(nameof(queryContext));
+    }
+
+    private readonly HashSet<string> aliasHashSet = new();
+
+    private string GenerateAlias(string name)
+    {
+      var streamAlias = name[0].ToString();
+
+      int i = 0;
+
+      var streamAliasAttempt = streamAlias;
+
+      while (aliasHashSet.Contains(streamAliasAttempt))
+      {
+        streamAliasAttempt = $"{streamAlias}{++i}";
+      }
+      
+      aliasHashSet.Add(streamAliasAttempt);
+
+      return streamAliasAttempt;
+    }
+
+    internal void VisitJoinTable(Expression[] expressions)
+    {
+      expressions = expressions.Select(StripQuotes).ToArray();
+      
+      Visit(expressions[0]);
+      var outerStreamAlias = GenerateAlias(queryContext.StreamName);
+
+      var streamAlias = GenerateAlias(streamName);
+
+      var lambdaExpression = expressions[3] as LambdaExpression;
+      var rewrittenAliases = PredicateReWriter.Rewrite(lambdaExpression, outerStreamAlias, streamAlias);
+
+      Append("SELECT ");
+
+      new KSqlJoinSelectFieldsVisitor(StringBuilder).Visit(rewrittenAliases);
+
+      AppendLine($" FROM {queryContext.StreamName} {outerStreamAlias}");
+
+      AppendLine($"INNER JOIN {streamName} {streamAlias}");
+      Append($"ON {outerStreamAlias}.");
+      Visit(expressions[1]);
+      Append($" = {streamAlias}.");
+      Visit(expressions[2]);
+      AppendLine(string.Empty);
+    }
+    
+    private string streamName;
+    
+    private static readonly IPluralize EnglishPluralizationService = new Pluralizer();
+
+    protected virtual string InterceptStreamName(string value)
+    {
+      if (contextOptions.ShouldPluralizeStreamName)
+        return EnglishPluralizationService.Pluralize(value);
+
+      return value;
+    }
+
+    protected override Expression VisitConstant(ConstantExpression constantExpression)
+    {
+      if (constantExpression == null) throw new ArgumentNullException(nameof(constantExpression));
+
+      if (constantExpression.Value is KStreamSet kStreamSet)
+      {
+        streamName = constantExpression.Type.BaseType?.GenericTypeArguments[0].Name;
+
+        streamName = kStreamSet?.QueryContext?.StreamName ?? InterceptStreamName(streamName);
+      }
+
+      return constantExpression;
+    }
+  }
+}
