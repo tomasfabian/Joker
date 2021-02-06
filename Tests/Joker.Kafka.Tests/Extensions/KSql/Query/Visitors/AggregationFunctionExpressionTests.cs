78f34dcedaac2ced97b6afb888c8b977526bf738
Tomas Fabian <tomas.fabian@colosseoeas.com>
2021-02-06 11:53:08 +0100

    [Kafka.DotNet.ksqlDB] - Aggregation functions - EarliestByOffset, LatestByOffset, EarliestByOffsetAllowNulls, LatestByOffsetAllowNull unit tests

--- Tests/Joker.Kafka.Tests/Extensions/KSql/Query/Visitors/AggregationFunctionExpressionTests.cs
+++ Tests/Joker.Kafka.Tests/Extensions/KSql/Query/Visitors/AggregationFunctionExpressionTests.cs
@@ -26,29 +26,81 @@ namespace Kafka.DotNet.ksqlDB.Tests.Extensions.KSql.Query.Visitors
     }
 
     [TestMethod]
-    public void Sqrt_BuildKSql_PrintsSqrtWithColumn()
+    public void Max_BuildKSql_PrintsMaxWithColumn()
     {
       //Arrange
-      Expression<Func<IKSqlGrouping<int, System.Drawing.Rectangle>, object>> expression = l => new { Key = l.Key, Sqrt = l.Sqrt(c => c.Height) };
+      Expression<Func<IKSqlGrouping<int, System.Drawing.Rectangle>, object>> expression = l => new { Key = l.Key, Max = l.Max(c => c.Height) };
 
       //Act
       var query = ClassUnderTest.BuildKSql(expression);
 
       //Assert
-      query.Should().BeEquivalentTo($"Key, SQRT({nameof(System.Drawing.Rectangle.Height)}) Sqrt");
+      query.Should().BeEquivalentTo($"Key, MAX({nameof(System.Drawing.Rectangle.Height)}) Max");
     }
 
     [TestMethod]
-    public void Sign_BuildKSql_PrintsSignWithColumn()
+    public void Min_BuildKSql_PrintsMinWithColumn()
     {
       //Arrange
-      Expression<Func<IKSqlGrouping<int, Transaction>, object>> expression = l => new { Key = l.Key, Sign = l.Sign(c => c.Amount) };
+      Expression<Func<IKSqlGrouping<int, Transaction>, object>> expression = l => new { Key = l.Key, Min = l.Min(c => c.Amount) };
 
       //Act
       var query = ClassUnderTest.BuildKSql(expression);
 
       //Assert
-      query.Should().BeEquivalentTo($"Key, SIGN({nameof(Transaction.Amount)}) Sign");
+      query.Should().BeEquivalentTo($"Key, MIN({nameof(Transaction.Amount)}) Min");
+    }
+
+    [TestMethod]
+    public void EarliestByOffset_BuildKSql_PrintsEarliestByOffsetWithColumn()
+    {
+      //Arrange
+      Expression<Func<IKSqlGrouping<int, Transaction>, object>> expression = l => new { Key = l.Key, EarliestByOffset = l.EarliestByOffset(c => c.Amount) };
+
+      //Act
+      var query = ClassUnderTest.BuildKSql(expression);
+
+      //Assert
+      query.Should().BeEquivalentTo($"Key, EARLIEST_BY_OFFSET({nameof(Transaction.Amount)}, true) EarliestByOffset");
+    }
+
+    [TestMethod]
+    public void EarliestByOffsetAllowNulls_BuildKSql_PrintsEarliestByOffsetAllowNullsWithColumn()
+    {
+      //Arrange
+      Expression<Func<IKSqlGrouping<int, Transaction>, object>> expression = l => new { Key = l.Key, EarliestByOffsetAllowNulls = l.EarliestByOffsetAllowNulls(c => c.Amount) };
+
+      //Act
+      var query = ClassUnderTest.BuildKSql(expression);
+
+      //Assert
+      query.Should().BeEquivalentTo($"Key, EARLIEST_BY_OFFSET({nameof(Transaction.Amount)}, false) EarliestByOffsetAllowNulls");
+    }
+
+    [TestMethod]
+    public void LatestByOffset_BuildKSql_PrintsLatestByOffsetWithColumn()
+    {
+      //Arrange
+      Expression<Func<IKSqlGrouping<int, Transaction>, object>> expression = l => new { Key = l.Key, LatestByOffset = l.LatestByOffset(c => c.Amount) };
+
+      //Act
+      var query = ClassUnderTest.BuildKSql(expression);
+
+      //Assert
+      query.Should().BeEquivalentTo($"Key, LATEST_BY_OFFSET({nameof(Transaction.Amount)}, true) LatestByOffset");
+    }
+
+    [TestMethod]
+    public void LatestByOffsetAllowNulls_BuildKSql_PrintsLatestByOffsetAllowNullsWithColumn()
+    {
+      //Arrange
+      Expression<Func<IKSqlGrouping<int, Transaction>, object>> expression = l => new { Key = l.Key, LatestByOffsetAllowNulls = l.LatestByOffsetAllowNulls(c => c.Amount) };
+
+      //Act
+      var query = ClassUnderTest.BuildKSql(expression);
+
+      //Assert
+      query.Should().BeEquivalentTo($"Key, LATEST_BY_OFFSET({nameof(Transaction.Amount)}, false) LatestByOffsetAllowNulls");
     }
   }
 }
