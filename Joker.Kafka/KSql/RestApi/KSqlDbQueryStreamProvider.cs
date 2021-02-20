dfc969a24ce7d8b30f2da9894c23e5530e1aa416
Tomas Fabian <tomas.fabian@colosseoeas.com>
2021-02-20 08:29:27 +0100

    [Kafka.DotNet.ksqlDB] - extracted RowValueJsonSerializer from KSqlDbQueryStreamProvider

--- Joker.Kafka/KSql/RestApi/KSqlDbQueryStreamProvider.cs
+++ Joker.Kafka/KSql/RestApi/KSqlDbQueryStreamProvider.cs
@@ -2,9 +2,7 @@
 using System.Net;
 using System.Net.Http;
 using System.Text.Json;
-using Kafka.DotNet.ksqlDB.Infrastructure.Extensions;
 using Kafka.DotNet.ksqlDB.KSql.RestApi.Exceptions;
-using Kafka.DotNet.ksqlDB.KSql.RestApi.Parsers;
 using Kafka.DotNet.ksqlDB.KSql.RestApi.Responses;
 
 namespace Kafka.DotNet.ksqlDB.KSql.RestApi
@@ -38,14 +36,16 @@ namespace Kafka.DotNet.ksqlDB.KSql.RestApi
       return httpClient;
     }
 
-    private QueryStreamHeader queryStreamHeader;
+    private RowValueJsonSerializer serializer;
 
     protected override RowValue<T> OnLineRed<T>(string rawJson)
     {
       //Console.WriteLine(rawJson);
       if (rawJson.Contains("queryId"))
       {
-        queryStreamHeader = JsonSerializer.Deserialize<QueryStreamHeader>(rawJson);
+        var queryStreamHeader = JsonSerializer.Deserialize<QueryStreamHeader>(rawJson);
+
+        serializer = new(queryStreamHeader);
       }      
       else if (rawJson.Contains("_error"))
       {
@@ -59,23 +59,13 @@ namespace Kafka.DotNet.ksqlDB.KSql.RestApi
       }
       else
       {
-        var result = rawJson.Substring(1, rawJson.Length - 2);
-        
         var jsonSerializerOptions = GetOrCreateJsonSerializerOptions();
 
-        if (queryStreamHeader.ColumnTypes.Length == 1 && !typeof(T).IsAnonymousType())
-          return new RowValue<T>(JsonSerializer.Deserialize<T>(result, jsonSerializerOptions));
-
-        var jsonRecord = new JsonArrayParser().CreateJson(queryStreamHeader.ColumnNames, result);
-
-        var record = JsonSerializer.Deserialize<T>(jsonRecord, jsonSerializerOptions);
-
-        return new RowValue<T>(record);
+        return serializer.Deserialize<T>(rawJson, jsonSerializerOptions);
       }
 
       return default;
     }
-
 
     protected override HttpRequestMessage CreateQueryHttpRequestMessage(HttpClient httpClient, object parameters)
     {
