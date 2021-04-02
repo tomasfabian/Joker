73b4549abaaf4b3322d21f42730cd7982235fe67
Tomas Fabian <tomas.fabian@colosseoeas.com>
2021-04-02 11:19:32 +0200

    [Kafka.DotNet.ksqlDB] - KSqlDBContext - execute string ksql queries

--- Joker.Kafka/KSql/Query/Context/IKSqlDBContext.cs
+++ Joker.Kafka/KSql/Query/Context/IKSqlDBContext.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
 using Kafka.DotNet.ksqlDB.KSql.Linq;
+using Kafka.DotNet.ksqlDB.KSql.RestApi.Parameters;
 
 namespace Kafka.DotNet.ksqlDB.KSql.Query.Context
 {
@@ -7,8 +10,10 @@ namespace Kafka.DotNet.ksqlDB.KSql.Query.Context
   {
 #if !NETSTANDARD
     IQbservable<TEntity> CreateQueryStream<TEntity>(string streamName = null);
+    IAsyncEnumerable<TEntity> CreateQueryStream<TEntity>(QueryStreamParameters queryStreamParameters, CancellationToken cancellationToken = default);
 #endif
 
     IQbservable<TEntity> CreateQuery<TEntity>(string streamName = null);
+    IAsyncEnumerable<TEntity> CreateQuery<TEntity>(QueryParameters queryParameters, CancellationToken cancellationToken = default);
   }
 }
--- Joker.Kafka/KSql/Query/Context/KSqlDBContext.cs
+++ Joker.Kafka/KSql/Query/Context/KSqlDBContext.cs
@@ -3,26 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using Kafka.DotNet.ksqlDB.Infrastructure.Extensions;
-using Kafka.DotNet.ksqlDB.KSql.Disposables;
 using Kafka.DotNet.ksqlDB.KSql.Linq;
-using Kafka.DotNet.ksqlDB.KSql.Query.Options;
 using Kafka.DotNet.ksqlDB.KSql.RestApi;
 using Kafka.DotNet.ksqlDB.KSql.RestApi.Parameters;
-#if !NETSTANDARD
-using System.Collections.Generic;
-using Kafka.DotNet.ksqlDB.KSql.RestApi.Parameters;
-#endif
-using Kafka.DotNet.ksqlDB.KSql.RestApi.Query;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace Kafka.DotNet.ksqlDB.KSql.Query.Context
 {
-  public class KSqlDBContext : AsyncDisposableObject, IKSqlDBContext
+  public class KSqlDBContext : KSqlDBContextDependenciesProvider, IKSqlDBContext
   {
     private readonly KSqlDBContextOptions contextOptions;
-    private readonly IServiceCollection serviceCollection;
 
     public KSqlDBContext(string ksqlDbUrl)
       : this(new KSqlDBContextOptions(ksqlDbUrl))
@@ -32,51 +23,24 @@ namespace Kafka.DotNet.ksqlDB.KSql.Query.Context
     public KSqlDBContext(KSqlDBContextOptions contextOptions)
     {
       this.contextOptions = contextOptions ?? throw new ArgumentNullException(nameof(contextOptions));
-
-      serviceCollection = new ServiceCollection();
-    }
-
-    private void RegisterDependencies(QueryType queryType)
-    {
-      OnConfigureServices(serviceCollection);
-
-      serviceCollection.TryAddScoped<IKSqlQbservableProvider, QbservableProvider>();
-
-      var uri = new Uri(contextOptions.Url);
-
-      serviceCollection.TryAddTransient<IKSqlQueryGenerator, KSqlQueryGenerator>();
-
-      if (!serviceCollection.HasRegistration<IHttpClientFactory>())
-        serviceCollection.AddSingleton<IHttpClientFactory, HttpClientFactory>(_ =>
-          new HttpClientFactory(uri));
-
-      switch (queryType)
-      {
-        case QueryType.Query:
-          serviceCollection.TryAddScoped<IKSqlDbProvider, KSqlDbQueryProvider>();
-          serviceCollection.TryAddSingleton(contextOptions.QueryParameters);
-          break;
-        case QueryType.QueryStream:
-          
-#if !NETSTANDARD
-          serviceCollection.TryAddScoped<IKSqlDbProvider, KSqlDbQueryStreamProvider>();
-          serviceCollection.TryAddSingleton<IQueryParameters>(contextOptions.QueryStreamParameters);
-          break;
-#else
-          throw new NotSupportedException();
-#endif
-      }
-
-      serviceCollection.TryAddSingleton(contextOptions);
-      serviceCollection.TryAddScoped<IKStreamSetDependencies, KStreamSetDependencies>();
     }
     
+    private readonly KSqlDBContextQueryDependenciesProvider KSqlDBQueryContext = new();
+    
 #if !NETSTANDARD
-    private ServiceProvider ServiceProvider { get; set; }
+
+    protected override void OnConfigureServices(IServiceCollection serviceCollection, KSqlDBContextOptions contextOptions)
+    {
+      base.OnConfigureServices(serviceCollection, contextOptions);
+          
+      serviceCollection.TryAddScoped<IKSqlDbProvider, KSqlDbQueryStreamProvider>();
+          
+      serviceCollection.TryAddSingleton<IQueryParameters>(contextOptions.QueryStreamParameters);
+    }
 
     public IAsyncEnumerable<TEntity> CreateQueryStream<TEntity>(QueryStreamParameters queryStreamParameters, CancellationToken cancellationToken = default)
     {
-      var serviceScopeFactory = Initialize();
+      var serviceScopeFactory = Initialize(contextOptions);
 
       var ksqlDBProvider = serviceScopeFactory.CreateScope().ServiceProvider.GetService<IKSqlDbProvider>();
 
@@ -85,7 +49,7 @@ namespace Kafka.DotNet.ksqlDB.KSql.Query.Context
 
     public IQbservable<TEntity> CreateQueryStream<TEntity>(string streamName = null)
     {
-      var serviceScopeFactory = Initialize();
+      var serviceScopeFactory = Initialize(contextOptions);
 
       if (streamName == String.Empty)
         streamName = null;
@@ -98,47 +62,11 @@ namespace Kafka.DotNet.ksqlDB.KSql.Query.Context
       return new KQueryStreamSet<TEntity>(serviceScopeFactory, queryStreamContext);
     }
 
-    private bool wasConfigured;
-
-    internal IServiceScopeFactory Initialize()
-    {
-      if (!wasConfigured)
-      {
-        wasConfigured = true;
-
-        RegisterDependencies(QueryType.QueryStream);
-
-        ServiceProvider = serviceCollection.BuildServiceProvider(new ServiceProviderOptions {ValidateScopes = true});
-      }
-
-      var serviceScopeFactory = ServiceProvider.GetService<IServiceScopeFactory>();
-
-      return serviceScopeFactory;
-    }
 #endif
-
-    protected virtual void OnConfigureServices(IServiceCollection serviceCollection)
-    {
-
-    }
-
-    private void RegisterHttpClientFactory<TFactory>()
-      where TFactory: class, IHttpClientFactory
-    {
-      serviceCollection.AddSingleton<IHttpClientFactory, TFactory>();
-    }
-
-    private void RegisterKSqlDbProvider<TProvider>()
-      where TProvider: class, IKSqlDbProvider
-    {
-      serviceCollection.AddScoped<IKSqlDbProvider, TProvider>();
-    }
-        
-    private ServiceProvider QueriesServiceProvider { get; set; }
     
     public IAsyncEnumerable<TEntity> CreateQuery<TEntity>(QueryParameters queryParameters, CancellationToken cancellationToken = default)
     {
-      var serviceScopeFactory = InitializeQueriesServiceProvider();
+      var serviceScopeFactory = KSqlDBQueryContext.Initialize(contextOptions);
 
       var ksqlDBProvider = serviceScopeFactory.CreateScope().ServiceProvider.GetService<IKSqlDbProvider>();
 
@@ -147,7 +75,7 @@ namespace Kafka.DotNet.ksqlDB.KSql.Query.Context
 
     public IQbservable<TEntity> CreateQuery<TEntity>(string streamName = null)
     {
-      var serviceScopeFactory = InitializeQueriesServiceProvider();
+      var serviceScopeFactory = KSqlDBQueryContext.Initialize(contextOptions);
 
       if (streamName == String.Empty)
         streamName = null;
@@ -159,33 +87,14 @@ namespace Kafka.DotNet.ksqlDB.KSql.Query.Context
 
       return new KQueryStreamSet<TEntity>(serviceScopeFactory, queryStreamContext);
     }
-    
-    private bool wereQueriesConfigured;
-
-    internal IServiceScopeFactory InitializeQueriesServiceProvider()
-    {
-      if (!wereQueriesConfigured)
-      {
-        wereQueriesConfigured = true;
-
-        RegisterDependencies(QueryType.Query);
-
-        QueriesServiceProvider = serviceCollection.BuildServiceProvider(new ServiceProviderOptions {ValidateScopes = true});
-      }
-
-      var serviceScopeFactory = QueriesServiceProvider.GetService<IServiceScopeFactory>();
-
-      return serviceScopeFactory;
-    }
 
     protected override async ValueTask OnDisposeAsync()
     {
 #if !NETSTANDARD
-      if(ServiceProvider != null)
-        await ServiceProvider.DisposeAsync();
+      await base.OnDisposeAsync();
 #endif
-      if(QueriesServiceProvider != null)
-        await QueriesServiceProvider.DisposeAsync();
+      if(KSqlDBQueryContext != null)
+        await KSqlDBQueryContext.DisposeAsync();
     }
   }
 }
