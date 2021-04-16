18f19e6a85201f5f2d28103c52cadfc9cc46b5e0
Tomas Fabian <tomas.fabian@colosseoeas.com>
2021-04-16 17:08:42 +0200

    [Kafka.DotNet.ksqlDB] - rest API provider - execute statements

---
+++ Joker.Kafka/KSql/RestApi/IKSqlDbRestApiProvider.cs
@@ -0,0 +1,12 @@
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Kafka.DotNet.ksqlDB.KSql.RestApi.Statements;
+
+namespace Kafka.DotNet.ksqlDB.KSql.RestApi
+{
+  public interface IKSqlDbRestApiProvider
+  {
+    Task<HttpResponseMessage> ExecuteStatementAsync(KSqlDbStatement ksqlStatement, CancellationToken cancellationToken = default);
+  }
+}
---
+++ Joker.Kafka/KSql/RestApi/KSqlDbRestApiProvider.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using Kafka.DotNet.ksqlDB.KSql.RestApi.Statements;
+
+namespace Kafka.DotNet.ksqlDB.KSql.RestApi
+{
+  public class KSqlDbRestApiProvider
+  {
+    private readonly IHttpClientFactory httpClientFactory;
+
+    public KSqlDbRestApiProvider(IHttpClientFactory httpClientFactory)
+    {
+      this.httpClientFactory = httpClientFactory ?? throw new ArgumentNullException(nameof(httpClientFactory));
+    }
+
+    private readonly string mediaType = "application/vnd.ksql.v1+json";
+
+    public async Task<HttpResponseMessage> ExecuteStatementAsync(KSqlDbStatement ksqlDbStatement, CancellationToken cancellationToken = default)
+    {
+      using var httpClient = httpClientFactory.CreateClient();
+      
+      var httpRequestMessage = CreateHttpRequestMessage(ksqlDbStatement);
+      
+      httpClient.DefaultRequestHeaders.Accept.Add(
+        new MediaTypeWithQualityHeaderValue(mediaType));
+
+      var httpResponseMessage = await httpClient.SendAsync(httpRequestMessage,
+        HttpCompletionOption.ResponseHeadersRead,
+        cancellationToken);
+			
+      return httpResponseMessage;
+    }
+
+    private HttpRequestMessage CreateHttpRequestMessage(KSqlDbStatement ksqlDbStatement)
+    {
+      var data = CreateContent(ksqlDbStatement);
+
+      var endpoint = GetEndpoint(ksqlDbStatement);
+
+      var httpRequestMessage = new HttpRequestMessage(HttpMethod.Post, endpoint)
+      {
+        Content = data
+      };
+
+      return httpRequestMessage;
+    }
+
+    private StringContent CreateContent(KSqlDbStatement ksqlDbStatement)
+    {
+      var statement = new KSqlStatement
+      {
+        ksql = ksqlDbStatement.StatementText
+      };
+
+      var json = JsonSerializer.Serialize(statement);
+
+      var data = new StringContent(json, ksqlDbStatement.ContentEncoding, mediaType);
+
+      return data;
+    }
+
+    private static string GetEndpoint(KSqlDbStatement ksqlDbStatement)
+    {
+      var endpoint = ksqlDbStatement.EndpointType switch
+      {
+        EndpointType.KSql => "/ksql",
+        EndpointType.Query => "/query",
+        _ => throw new ArgumentOutOfRangeException()
+      };
+
+      return endpoint;
+    }
+  }
+}
