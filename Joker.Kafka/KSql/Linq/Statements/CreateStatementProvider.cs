92a116db46600c913dbb2f2d9022dfa30192f0c7
Tomas Fabian <tomas.fabian@colosseoeas.com>
2021-04-24 10:42:26 +0200

    [ksqldb] - CreateStatementExtensions

---
+++ Joker.Kafka/KSql/Linq/Statements/CreateStatementProvider.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq.Expressions;
+using Kafka.DotNet.ksqlDB.KSql.Query;
+using Kafka.DotNet.ksqlDB.KSql.Query.Context;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Kafka.DotNet.ksqlDB.KSql.Linq.Statements
+{
+  internal class CreateStatementProvider : ICreateStatementProvider
+  {
+    private readonly IServiceScopeFactory serviceScopeFactory;
+    private readonly QueryContext queryContext;
+
+    public CreateStatementProvider(IServiceScopeFactory serviceScopeFactory, QueryContext queryContext = null)
+    {
+      this.serviceScopeFactory = serviceScopeFactory ?? throw new ArgumentNullException(nameof(serviceScopeFactory));
+      this.queryContext = queryContext;
+    }
+    
+    ICreateStatement<TResult> ICreateStatementProvider.CreateStatement<TResult>(Expression expression)
+    {
+      return new CreateStatement<TResult>(serviceScopeFactory, expression, queryContext);
+    }
+  }
+}
