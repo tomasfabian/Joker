a6983d98aa989a24382cc1d77cc21e155ddc298e
Tomas Fabian <tomas.fabian@colosseoeas.com>
2021-05-03 16:28:57 +0200

    [ksqldb] - PullQueryExtensions unit tests

--- Tests/Joker.Kafka.Tests/Extensions/KSql/Linq/PullQueries/PullQueryExtensionsTests.cs
+++ Tests/Joker.Kafka.Tests/Extensions/KSql/Linq/PullQueries/PullQueryExtensionsTests.cs
@@ -1,6 +1,7 @@
 ﻿using System.Threading.Tasks;
 using FluentAssertions;
 using Kafka.DotNet.ksqlDB.KSql.Linq.PullQueries;
+using Kafka.DotNet.ksqlDB.KSql.Query.Functions;
 using Kafka.DotNet.ksqlDB.KSql.RestApi;
 using Kafka.DotNet.ksqlDB.Tests.Fakes.Http;
 using Kafka.DotNet.ksqlDB.Tests.Helpers;
@@ -83,6 +84,44 @@ WHERE SensorId = '{sensorId}';");
       //Assert
       ksql.Should().BeEquivalentTo(@$"SELECT * FROM {MaterializedViewName}
 WHERE SensorId = '{sensorId}';");
+    }
+
+    [TestMethod]
+    public void CreatePullQuery_WindowBounds_LongTypeDateFormat()
+    {
+      //Arrange
+      string sensorId = "sensor-1";      
+      var query = DbProvider.CreatePullQuery<IoTSensorStats>(MaterializedViewName)
+        .Where(c => c.SensorId == sensorId);
+
+      long windowStart = 1575044700000;
+      long windowEnd = 1675044700000;
+
+      //Act
+      var ksql = query.Where(c => Bounds.WindowStart > windowStart && Bounds.WindowEnd <= windowEnd).ToQueryString();
+
+      //Assert
+      ksql.Should().BeEquivalentTo(@$"SELECT * FROM {MaterializedViewName}
+WHERE SensorId = '{sensorId}' AND (WINDOWSTART > {windowStart}) AND (WINDOWEND <= {windowEnd});");
+    }
+
+    [TestMethod]
+    public void CreatePullQuery_WindowBounds_StringTypeDateFormat()
+    {
+      //Arrange
+      string sensorId = "sensor-1";      
+      var query = DbProvider.CreatePullQuery<IoTSensorStats>(MaterializedViewName)
+        .Where(c => c.SensorId == sensorId);
+
+      string windowStart = "2019-10-03T21:31:16";
+      string windowEnd = "2020-10-03T21:31:16";
+
+      //Act
+      var ksql = query.Where(c => Bounds.WindowStart > windowStart && Bounds.WindowEnd <= windowEnd).ToQueryString();
+
+      //Assert
+      ksql.Should().BeEquivalentTo(@$"SELECT * FROM {MaterializedViewName}
+WHERE SensorId = '{sensorId}' AND (WINDOWSTART > '{windowStart}') AND (WINDOWEND <= '{windowEnd}');");
     }
   }
 }
