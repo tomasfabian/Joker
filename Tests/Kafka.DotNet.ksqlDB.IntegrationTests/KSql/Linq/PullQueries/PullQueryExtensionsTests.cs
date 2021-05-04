45e320d1c08da08dadfd6d8349e2559b2548d730
Tomas Fabian <tomas.fabian@colosseoeas.com>
2021-05-04 07:37:59 +0200

    [ksqldb] - PullQueryExtensions unit tests

--- Tests/Kafka.DotNet.ksqlDB.IntegrationTests/KSql/Linq/PullQueries/PullQueryExtensionsTests.cs
+++ Tests/Kafka.DotNet.ksqlDB.IntegrationTests/KSql/Linq/PullQueries/PullQueryExtensionsTests.cs
@@ -4,6 +4,7 @@ using Kafka.DotNet.ksqlDB.IntegrationTests.KSql.RestApi;
 using Kafka.DotNet.ksqlDB.IntegrationTests.Models.Sensors;
 using Kafka.DotNet.ksqlDB.KSql.Linq.PullQueries;
 using Kafka.DotNet.ksqlDB.KSql.Query.Context;
+using Kafka.DotNet.ksqlDB.KSql.Query.Functions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Kafka.DotNet.ksqlDB.IntegrationTests.KSql.Linq.PullQueries
@@ -37,6 +38,28 @@ namespace Kafka.DotNet.ksqlDB.IntegrationTests.KSql.Linq.PullQueries
       //Act
       var result = await context.CreatePullQuery<IoTSensorStats>(SensorsPullQueryProvider.MaterializedViewName)
         .Where(c => c.SensorId == sensorId)
+        .GetAsync();
+      
+      //Assert
+      result.Should().NotBeNull();
+      result.SensorId.Should().Be(sensorId);
+      result.WindowStart.Should().NotBe(null);
+      result.WindowEnd.Should().NotBe(null);
+    }
+
+    [TestMethod]
+    public async Task CreatePullQuery_WithBounds()
+    {
+      //Arrange
+      string sensorId = "sensor-1";
+
+      string windowStart = "2019-10-03T21:31:16";
+      string windowEnd = "2225-10-03T21:31:16";
+
+      //Act
+      var result = await context.CreatePullQuery<IoTSensorStats>(SensorsPullQueryProvider.MaterializedViewName)
+        .Where(c => c.SensorId == sensorId)
+        .Where(c => Bounds.WindowStart > windowStart && Bounds.WindowEnd <= windowEnd)
         .GetAsync();
       
       //Assert
