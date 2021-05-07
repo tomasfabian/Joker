bfeee917ddb12aa5bb2656276fba21c85c20d744
Tomas Fabian <tomas.fabian@colosseoeas.com>
2021-05-07 07:35:22 +0200

    [ksqldb] - CreateOrReplace[Stream/table] rest api provider

--- Joker.Kafka/KSql/RestApi/IKSqlDbRestApiClient.cs
+++ Joker.Kafka/KSql/RestApi/IKSqlDbRestApiClient.cs
@@ -8,5 +8,9 @@ namespace Kafka.DotNet.ksqlDB.KSql.RestApi
   public interface IKSqlDbRestApiClient
   {
     Task<HttpResponseMessage> ExecuteStatementAsync(KSqlDbStatement ksqlStatement, CancellationToken cancellationToken = default);
+    Task<HttpResponseMessage> CreateTable<T>(EntityCreationMetadata creationMetadata, CancellationToken cancellationToken = default);
+    Task<HttpResponseMessage> CreateStream<T>(EntityCreationMetadata creationMetadata, CancellationToken cancellationToken = default);
+    Task<HttpResponseMessage> CreateOrReplaceTable<T>(EntityCreationMetadata creationMetadata, bool ifNotExists = false, CancellationToken cancellationToken = default);
+    Task<HttpResponseMessage> CreateOrReplaceStream<T>(EntityCreationMetadata creationMetadata, bool ifNotExists = false, CancellationToken cancellationToken = default);
   }
 }
--- Joker.Kafka/KSql/RestApi/KSqlDbRestApiClient.cs
+++ Joker.Kafka/KSql/RestApi/KSqlDbRestApiClient.cs
@@ -4,6 +4,8 @@ using System.Net.Http.Headers;
 using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
+using Kafka.DotNet.ksqlDB.KSql.Query.Context;
+using Kafka.DotNet.ksqlDB.KSql.RestApi.Enums;
 using Kafka.DotNet.ksqlDB.KSql.RestApi.Statements;
 
 namespace Kafka.DotNet.ksqlDB.KSql.RestApi
@@ -30,7 +32,7 @@ namespace Kafka.DotNet.ksqlDB.KSql.RestApi
 
       var httpResponseMessage = await httpClient.SendAsync(httpRequestMessage, cancellationToken)
         .ConfigureAwait(false);
-			
+
       return httpResponseMessage;
     }
 
@@ -68,5 +70,62 @@ namespace Kafka.DotNet.ksqlDB.KSql.RestApi
 
       return endpoint;
     }
+
+    #region Creation
+    
+    public Task<HttpResponseMessage> CreateOrReplaceStream<T>(EntityCreationMetadata creationMetadata, bool ifNotExists = false, CancellationToken cancellationToken = default)
+    {
+      var statementContext = new StatementContext
+      {
+        CreationType = CreationType.CreateOrReplace,
+        KSqlEntityType = KSqlEntityType.Stream
+      };
+
+      return CreateOrReplace<T>(statementContext, creationMetadata, ifNotExists, cancellationToken);
+    }
+
+    public Task<HttpResponseMessage> CreateOrReplaceTable<T>(EntityCreationMetadata creationMetadata, bool ifNotExists = false, CancellationToken cancellationToken = default)
+    {
+      var statementContext = new StatementContext
+      {
+        CreationType = CreationType.CreateOrReplace,
+        KSqlEntityType = KSqlEntityType.Table
+      };
+
+      return CreateOrReplace<T>(statementContext, creationMetadata, ifNotExists, cancellationToken);
+    }
+
+    public Task<HttpResponseMessage> CreateStream<T>(EntityCreationMetadata creationMetadata, CancellationToken cancellationToken = default)
+    {
+      var statementContext = new StatementContext
+      {
+        CreationType = CreationType.Create,
+        KSqlEntityType = KSqlEntityType.Stream
+      };
+
+      return CreateOrReplace<T>(statementContext, creationMetadata, ifNotExists: null, cancellationToken);
+    }
+
+    public Task<HttpResponseMessage> CreateTable<T>(EntityCreationMetadata creationMetadata, CancellationToken cancellationToken = default)
+    {
+      var statementContext = new StatementContext
+      {
+        CreationType = CreationType.Create,
+        KSqlEntityType = KSqlEntityType.Table
+      };
+
+      return CreateOrReplace<T>(statementContext, creationMetadata, ifNotExists: null, cancellationToken);
+    }
+
+    private Task<HttpResponseMessage> CreateOrReplace<T>(StatementContext statementContext, EntityCreationMetadata creationMetadata, bool? ifNotExists, CancellationToken cancellationToken = default)
+    {
+      string ksql = new CreateEntity().Print<T>(statementContext, creationMetadata, ifNotExists);
+
+      var ksqlStatement = new KSqlDbStatement(ksql);
+
+      return ExecuteStatementAsync(ksqlStatement, cancellationToken);
+    }
+
+    #endregion
   }
 }
