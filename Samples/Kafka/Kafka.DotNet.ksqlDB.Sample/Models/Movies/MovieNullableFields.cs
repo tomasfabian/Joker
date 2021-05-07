1b87decf402e6b621f69ad8613f54a28b9f7f0e4
Tomas Fabian <tomas.fabian@colosseoeas.com>
2021-05-07 17:20:20 +0200

    [ksqldb] - Poco key attribute example

--- Samples/Kafka/Kafka.DotNet.ksqlDB.Sample/Models/Movies/MovieNullableFields.cs
+++ Samples/Kafka/Kafka.DotNet.ksqlDB.Sample/Models/Movies/MovieNullableFields.cs
@@ -1,8 +1,11 @@
-﻿namespace Kafka.DotNet.ksqlDB.Sample.Models.Movies
+﻿using Kafka.DotNet.ksqlDB.KSql.RestApi.Statements.Annotations;
+
+namespace Kafka.DotNet.ksqlDB.Sample.Models.Movies
 {
   public class MovieNullableFields
   {
-    public string Title { get; set; }
+    public string Title { get; set; }    
+    [Key]
     public int? Id { get; set; }
     public int? Release_Year { get; set; }
   }
