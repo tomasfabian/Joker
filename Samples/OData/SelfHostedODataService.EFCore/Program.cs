0f0edcd423d42e8ad930114e7e88083234f68931
Tomas Fabian <t.fabian@cid.com>
2024-02-08 07:43:38 +0100

    [Joker.OData] - program file migrated to Top-level statements

--- Samples/OData/SelfHostedODataService.EFCore/Program.cs
+++ Samples/OData/SelfHostedODataService.EFCore/Program.cs
@@ -1,63 +1,54 @@
 ﻿using System;
-using System.Threading.Tasks;
 using Joker.OData.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using SelfHostedODataService.EFCore;
 using SelfHostedODataService.EFCore.HostedServices;
 using SelfHostedODataService.HostedServices;
 using Serilog;
 
-namespace SelfHostedODataService.EFCore
+var startupSettings = new KestrelODataWebHostConfig()
 {
-  public class Program
+  ConfigureServices = services =>
   {
-    public static async Task Main(string[] args)
-    {
-      var startupSettings = new KestrelODataWebHostConfig()
-      {
-        ConfigureServices = services =>
-        {
-          services.AddHostedService<ProductChangesHostedService>();
-          services.AddHostedService<SqlTableDependencyProviderHostedService>();
-        }
-      };
-      
-      ConfigureLogging();
-      
-      await new ODataHost().RunAsync(args, startupSettings);
-    }
-
-    private static void ConfigureLogging()
-    {
-      var baseDir = AppDomain.CurrentDomain.BaseDirectory;
-
-      Log.Logger = new LoggerConfiguration()
-        .MinimumLevel.Debug()
-        .WriteTo.Console()
-        .WriteTo.File($@"{baseDir}\logs\{nameof(ODataHost)}_.txt", rollingInterval: RollingInterval.Day)
-        .CreateLogger();
-
-      Log.Information("Hello, world!");
-    }
-
-    #region IISODataWebHostConfig example
-
-    private static ODataWebHostConfig ODataStartupConfigExample()
-    {
-      var configuration = new ConfigurationBuilder()
-        .AddEnvironmentVariables()
-        .Build();
-
-      var startupSettings = new IISODataWebHostConfig()
-      {
-        ConfigureServices = services => { services.AddHostedService<SqlTableDependencyProviderHostedService>(); },
-        Urls = new[] { @"https://localhost:32778/" },
-        Configuration = configuration
-      };
-
-      return startupSettings;
-    }
-
-    #endregion
+    services.AddHostedService<ProductChangesHostedService>();
+    services.AddHostedService<SqlTableDependencyProviderHostedService>();
   }
+};
+
+ConfigureLogging();
+
+await new ODataHost().RunAsync(args, startupSettings);
+
+static void ConfigureLogging()
+{
+  var baseDir = AppDomain.CurrentDomain.BaseDirectory;
+
+  Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.Console()
+    .WriteTo.File($@"{baseDir}\logs\{nameof(ODataHost)}_.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
+  Log.Information("Hello, world!");
 }
+
+#region IISODataWebHostConfig example
+
+static ODataWebHostConfig ODataStartupConfigExample()
+{
+  var configuration = new ConfigurationBuilder()
+    .AddEnvironmentVariables()
+    .Build();
+
+  var startupSettings = new IISODataWebHostConfig()
+  {
+    ConfigureServices = services => { services.AddHostedService<SqlTableDependencyProviderHostedService>(); },
+    Urls = ["https://localhost:32778/"],
+    Configuration = configuration
+  };
+
+  return startupSettings;
+}
+
+#endregion
